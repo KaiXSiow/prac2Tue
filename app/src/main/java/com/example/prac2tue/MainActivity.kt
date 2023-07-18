150ce79659cc4fe696dadb74b0cf6323824fffe5
KaiXSiow <siowchuankai442001@gmail.com>
2023-07-18 18:10:50 +0800

    initial commit

--- app/src/main/java/com/example/prac2tue/MainActivity.kt
+++ app/src/main/java/com/example/prac2tue/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.prac2tue
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
